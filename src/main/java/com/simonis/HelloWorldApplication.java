eec5e0108a9e27c7f8193ddc1927b9838f6a528c
unknown <hien.nq83@gmail.com>
2019-05-06 00:47:34 +0700

    preconfig with war format

--- src/main/java/com/simonis/HelloWorldApplication.java
+++ src/main/java/com/simonis/HelloWorldApplication.java
@@ -2,11 +2,18 @@ package com.simonis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloWorldApplication {
+public class HelloWorldApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HelloWorldApplication.class);
+    }
 }
